6cc6119cdcf1e147792a7e3376cd666fd7ba7dfc
EC2 Default User <ec2-user@ip-172-31-30-160.ec2.internal>
2021-05-01 13:03:03 +0000

    [Start]function of comment

---
+++ README.md
@@ -0,0 +1,5 @@
+DMM Webcamp Bookers2へ「いいね/コメント機能」を実装の課題です。
+
+railsファイルで、製作者がサーバーを立ち上げている場合のみ、以下URLからアクセスできます。
+
+https://48de7d062b464c52959c22fe473e7921.vfs.cloud9.us-east-1.amazonaws.com/
---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :book_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only:[:show, :edit, :update, :index,]
+  get 'home/about', to: 'homes#show'
+
+  get 'books/:id/edit' => 'books#edit'
+  post 'books/:id/edit' => 'books#edit'
+
+  post 'users/:id' => 'books#create'
+  post 'users' => 'books#create'
+  post 'books/:id' => 'books#create'
+
+  get 'users/:id/edit' => 'users#edit'
+  patch 'users/:id' => 'users#update'
+end
